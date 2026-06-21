bff1fafa039cf6b534a623af7b6c83df963db9c0
Aditya joshi <adityajoshi1602@gmail.com>
2026-06-21 10:29:12 +0530

    feat: implement daily, weekly, monthly, and yearly return calculations

--- src/returns.py
+++ src/returns.py
@@ -2,3 +2,15 @@ import numpy as np
 
 def log_ret(prices):
     return np.log(prices / prices.shift(1)).dropna()
+
+def week_ret(prices):
+    week = prices.resample("W").last()
+    return week.pct_change().dropna()
+
+def month_ret(prices):
+    month = prices.resample("ME").last()
+    return month.pct_change().dropna()
+
+def year_ret(prices):
+    year = prices.resample("YE").last()
+    return year.pct_change().dropna()
